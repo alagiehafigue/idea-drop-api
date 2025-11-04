092d577b072bb21a0daac2c172b8d919f4869086
alagiehafigue <alagiehafigue@gmail.com>
2025-11-04 14:50:45 +0000

    Add a customer error handler middleware

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
 import ideaRouter from "./routes/ideaRoutes.js";
+import { errroHandler } from "./middleware/errorHandler.js";
 
 dotenv.config();
 
@@ -12,6 +13,16 @@ app.use(cors());
 app.use(express.urlencoded({ extended: true }));
 
 app.use("/api/ideas", ideaRouter);
+
+//404 Fall Back
+app.use((req, res, next) => {
+  const error = new Error(`Not Found - ${req.originalUrl}`);
+  res.status(404);
+
+  next(error);
+});
+
+app.use(errroHandler);
 
 app.listen(PORT, () => {
   console.log(`Server is running on ${PORT}`);
