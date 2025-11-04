7e829d2addd436f7ad5e35cb0c7afa9b11703417
alagiehafigue <alagiehafigue@gmail.com>
2025-11-04 13:49:45 +0000

    create a routes file

---
+++ routes/ideaRoutes.js
@@ -0,0 +1,26 @@
+import express from "express";
+
+const router = express.Router();
+
+//@route     Get /api/ideas
+//@description Get all ideas
+//@access       Public
+router.get("/", (req, res) => {
+  const ideas = [
+    { id: 1, title: "idea 1", descrption: "This is idea 1" },
+    { id: 2, title: "idea 2", descrption: "This is idea 2" },
+    { id: 3, title: "idea 3", descrption: "This is idea 3" },
+  ];
+
+  res.json(ideas);
+});
+
+//@route     Post /api/ideas
+//@description Post an idea
+//@access       Public
+router.post("/", (req, res) => {
+  const { title, descrption } = req.body;
+  res.send(title);
+});
+
+export default router;
