99b35e2f999ecfeb549d8f0c381e7611ad5bfad2
alagiehafigue <alagiehafigue@gmail.com>
2025-11-13 02:24:08 +0000

    Generate tokens for auth routes

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,6 +1,7 @@
 import express from "express";
 import mongoose from "mongoose";
 import User from "../models/User.js";
+import { generateToken } from "../utils/generateToken.js";
 
 const router = express.Router();
 
@@ -26,7 +27,22 @@ router.post("/register", async (req, res, next) => {
 
     const user = await User.create({ name, email, password });
 
+    // Create Tokens
+    const payload = { userId: user._id.toString() };
+    const accessToken = await generateToken(payload, "1m");
+    const refreshToken = await generateToken(payload, "30d");
+
+    // set refresh token in HTTP-only cookie
+
+    res.cookie("refreshToken", refreshToken, {
+      httpOnly: true,
+      secure: process.env.NODE_ENV === "production",
+      sameSite: "none",
+      maxAge: 30 * 24 * 60 * 60 * 1000, // 30 days
+    });
+
     res.status(201).json({
+      accessToken,
       user: {
         id: user._id,
         name: user.name,
